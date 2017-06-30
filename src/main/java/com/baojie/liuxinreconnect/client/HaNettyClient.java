4647d7184e87db0783fc859ec66bc214b6d68553
baojie5642 <1039245098@qq.com>
2017-06-30 10:04:39 +0800

    nettymanager第一次完整版，但是性能一般，因为里面使用了大量的map，没有数组高效，考虑之后重构成数组，然后在缓存方面比如future上提高使用效率

--- src/main/java/com/baojie/liuxinreconnect/client/HaNettyClient.java
+++ src/main/java/com/baojie/liuxinreconnect/client/HaNettyClient.java
@@ -301,7 +301,7 @@ public class HaNettyClient {
             e.printStackTrace();
         }
         try {
-            TimeUnit.SECONDS.sleep(6);
+            TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
